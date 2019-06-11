ce83205a0d447482a66235f4d8ebd5c4e5601b5c
jidemobell <jidd32@gmail.com>
2019-06-12 00:45:01 +0100

    Updates

---
+++ precache-manifest.f4afc9d48bf63291509f8d0cc5e439a6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ceaa92fe20ff37d8242a6763e16d7922",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "7755c75848de843ce6b5",
+    "url": "/portfolio/static/css/2.dbd8b1e0.chunk.css"
+  },
+  {
+    "revision": "3737193e6f7949b56fdf",
+    "url": "/portfolio/static/css/main.2297398f.chunk.css"
+  },
+  {
+    "revision": "7755c75848de843ce6b5",
+    "url": "/portfolio/static/js/2.ccf10637.chunk.js"
+  },
+  {
+    "revision": "3737193e6f7949b56fdf",
+    "url": "/portfolio/static/js/main.a06f2173.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "48acff3fae0274fb2af8f996915d93c4",
+    "url": "/portfolio/static/media/edited.48acff3f.png"
+  }
+]);
