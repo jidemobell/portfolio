d939f84e7f1dce7648553e5ee71ff7e0d6703321
jidemobell <jidd32@gmail.com>
2019-11-26 23:33:34 +0100

    Updates

--- precache-manifest.3107c32defb70bf34f37a0a38b510d08.js
+++ precache-manifest.3107c32defb70bf34f37a0a38b510d08.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "b7907b6ee7e68ead0a77073292f06923",
+    "revision": "efa3e4295dbfd3f24e592f5576e5f2e8",
     "url": "/portfolio/index.html"
   },
   {
@@ -8,16 +8,16 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/portfolio/static/css/2.cbc5b225.chunk.css"
   },
   {
-    "revision": "ae4aa63d1f9746c65dcb",
-    "url": "/portfolio/static/css/main.8dac4618.chunk.css"
+    "revision": "7aad81f8393bb5e0b549",
+    "url": "/portfolio/static/css/main.b0f02827.chunk.css"
   },
   {
     "revision": "661017d178db56b7edfb",
     "url": "/portfolio/static/js/2.94603309.chunk.js"
   },
   {
-    "revision": "ae4aa63d1f9746c65dcb",
-    "url": "/portfolio/static/js/main.593e0192.chunk.js"
+    "revision": "7aad81f8393bb5e0b549",
+    "url": "/portfolio/static/js/main.15d82a72.chunk.js"
   },
   {
     "revision": "53ebc025fca807d4dbea",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.4f689d11da882fc2e2e1a7074d614aa3.js"
+  "/portfolio/precache-manifest.3107c32defb70bf34f37a0a38b510d08.js"
 );
 
 self.addEventListener('message', (event) => {
