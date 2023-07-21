2cad439c2d2440cd74f4c8ae014541d706915467
Olajide Olaniyan <jidd32@gmail.com>
2023-07-21 15:30:19 +0100

    update target

--- src/components/Journal/Journal.js
+++ src/components/Journal/Journal.js
@@ -2,7 +2,7 @@ import Header from "../Header/Header";
 import style from "./journal.module.css";
 import React, { useEffect, useState } from "react";
 
-export default function Journal({closeJournal}) {
+export default function Journal({ closeJournal }) {
   const [data, setData] = useState([]);
 
   async function getBlog() {
@@ -10,7 +10,6 @@ export default function Journal({closeJournal}) {
       "https://dev.to/api/articles?username=jidemobell"
     );
     const blogs = await response.json();
-    console.log(blogs);
     setData(blogs);
   }
 
@@ -18,20 +17,27 @@ export default function Journal({closeJournal}) {
     getBlog();
   }, []);
   return (
-    
     <>
       <Header fromJournal={true} passCloseAction={closeJournal} />
       <div className={style.journalcontainer}>
         <section className={style.sectcontainer}>
           {data.map((entry) => {
             return (
-                <div className="hvr-sink  blogs" style={{background:"#181818",padding:"20px", marginTop: "20px"}}  key={entry.id}>
-                <a href={entry.url} className={  style.link} key={entry.id}>
+              <div
+                className="hvr-sink  blogs"
+                style={{
+                  background: "#181818",
+                  padding: "20px",
+                  marginTop: "20px",
+                }}
+                key={entry.id}
+              >
+                <a href={entry.url} className={style.link} key={entry.id} target="_blank" rel="noopener noreferrer">
                   <div className={style.top}>
                     <div className={style.date}> {entry.created_at} </div>
                     <div className={style.time}> 12:08 PM </div>
                   </div>
-                  <div className={style.bottom} >
+                  <div className={style.bottom}>
                     <span>{entry.title}</span>
                   </div>
                 </a>
